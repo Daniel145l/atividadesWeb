40ca8051fc58aed7baab927e7d14351785b2dd5b
Daniel Lucas <daniellucasm18@gmail.com>
2025-06-26 00:21:36 -0300

    enviando atividade correta para a branch correta

---
+++ alunos_api/data/alunos.js
@@ -0,0 +1,7 @@
+const alunos = [
+  { id: 1, nome: 'Daniel', curso: 'Engenharia de Software', ira: 9.2 },
+  { id: 2, nome: 'Maria', curso: 'Ciência da Computação', ira: 7.5 },
+  { id: 3, nome: 'João', curso: 'Sistemas de Informação', ira: 8.5 },
+];
+
+module.exports = alunos;
